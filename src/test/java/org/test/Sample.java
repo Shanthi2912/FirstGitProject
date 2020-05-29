d5e318a943c72004a2d8b5b82328ddb375eed05e
shanthi2912 <shanthibindu29@gmail.com>
2020-05-29 20:34:15 +0530

    Bindu code is pushed

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -3,12 +3,10 @@ package org.test;
 public class Sample {
 
 	public static void main(String[] args) {
-
-System.out.println("Welcome");
-	System.out.println("hai");
-	System.out.println("hello");
-System.out.println("Hello");
-System.out.println("hi");	
+		System.out.println("Hello");
+		System.out.println("hi");	
+    System.out.println("welcome");
+	
 
 
 	}
